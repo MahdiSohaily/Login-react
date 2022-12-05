4100b95dcfee5443c812365d27381085cf867552
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 09:39:23 +0430

    Add login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,16 @@
 import './styles.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form method="post">
+        <input type="text" placeholder="Username" required="required" />
+        <input type="password" placeholder="Password" required="required" />
+        <button type="submit" className="btn btn-primary btn-block btn-large">
+          Let me in.
+        </button>
+      </form>
+    </div>
+  );
 }
