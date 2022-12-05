109de0f48ab4159738b2b7d03d8711c5483f73d3
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 10:25:34 +0430

    Add login form states

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import './style.css';
 
 export default function Login() {
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="login">
       <h1>Login</h1>
       <form method="post">
-        <input type="text" placeholder="Username" required="required" />
-        <input type="password" placeholder="Password" required="required" />
+        <input
+          name="username"
+          type="text"
+          placeholder="Username"
+          required="required"
+          value={input.username}
+          onChange={handleChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required="required"
+          value={input.password}
+          onChange={handleChange}
+        />
         <button type="submit" className="btn btn-primary btn-block btn-large">
           Let me in.
         </button>
