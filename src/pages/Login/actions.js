edff9abe4f9950e2b4d6e4176b27296621f48362
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 20:27:08 +0430

    Add function to get cookies

--- src/pages/Login/actions.js
+++ src/pages/Login/actions.js
@@ -25,3 +25,19 @@ export function setCookie(cname, cValue, exDays) {
   const expires = `expires=${d.toUTCString()}`;
   document.cookie = `${cname}=${cValue};${expires};path=/`;
 }
+
+export function getCookie(cname) {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i += 1) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
