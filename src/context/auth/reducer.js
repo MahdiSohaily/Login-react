037e699263a30de352282a1d4988e77add2c6357
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 10:06:27 +0430

    Add context reducer

--- src/context/auth/reducer.js
+++ src/context/auth/reducer.js
@@ -11,3 +11,43 @@ export const actionTypes = {
   LOGIN_ERROR: 'LOGIN_ERROR',
   LOGOUT: 'LOGOUT',
 };
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case actionTypes.LOGIN_REQUEST:
+      return {
+        ...state,
+        user: null,
+        token: null,
+        loading: true,
+        error: null,
+      };
+    case actionTypes.LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+        loading: false,
+        error: null,
+      };
+    case actionTypes.LOGIN_ERROR:
+      return {
+        ...state,
+        user: null,
+        token: null,
+        loading: false,
+        error: action.payload.error,
+      };
+    case actionTypes.LOGOUT:
+      return {
+        ...state,
+        user: null,
+        token: null,
+        loading: false,
+        error: null,
+      };
+
+    default:
+      throw new Error('Action type not supported');
+  }
+}
