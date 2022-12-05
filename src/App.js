319524da16b6fc54696223ce7eab02953201d67b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 10:17:53 +0430

    Add script to check for token and render correct component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useAuthState } from './context/auth/auth-context';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
-  return <Login />;
+  const { token } = useAuthState();
+  return token ? <Dashboard /> : <Login />;
 }
 
 export default App;
