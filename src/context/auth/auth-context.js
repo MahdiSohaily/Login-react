6e24b07f3ee88fe68bdc20c29eda91d3fe3041ae
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 09:55:35 +0430

    Add context using custom hooks

--- src/context/auth/auth-context.js
+++ src/context/auth/auth-context.js
@@ -1,8 +1,26 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 
 const AuthStateContext = React.createContext();
 const AuthDespatcherContext = React.createContext();
+
+export function useAuthState() {
+  const context = useContext(AuthStateContext);
+  if (!context) {
+    throw new Error('AuthStateContext provider must be used !!!');
+  }
+
+  return context;
+}
+
+export function useAuthDispatch() {
+  const context = useContext(AuthDespatcherContext);
+  if (!context) {
+    throw new Error('AuthDespatcherContext provider must be used !!!');
+  }
+
+  return context;
+}
 
 export default function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
