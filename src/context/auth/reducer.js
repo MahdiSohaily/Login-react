37dcd279d8bbf3835c5a79c850d81fdc5b4cf3ed
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-12-05 19:35:44 +0430

    Add logout action creator

--- src/context/auth/reducer.js
+++ src/context/auth/reducer.js
@@ -1,3 +1,8 @@
+/**
+ * this is an object which define the values
+ * which we need as a state for preforming
+ * actions that is required in login action.
+ */
 export const initialState = {
   user: null,
   token: null,
@@ -5,6 +10,11 @@ export const initialState = {
   error: null,
 };
 
+/**
+ * this is an ENUM like object that indicate
+ * to the different actions user can preform
+ * in the login form page.
+ */
 export const actionTypes = {
   LOGIN_REQUEST: 'LOGIN_REQUEST',
   LOGIN_SUCCESS: 'LOGIN_SUCCESS',
@@ -12,6 +22,15 @@ export const actionTypes = {
   LOGOUT: 'LOGOUT',
 };
 
+/**
+ * This is a reducer function which handle the user
+ * different actions in the login form for login action
+ * @param {object} state is a global object that indicate
+ * to the state of our application
+ * @param {object} action an object indicate to the action type
+ * and provided data from the user to change global state
+ * @returns the new state of our application based on user action
+ */
 export default function reducer(state, action) {
   switch (action.type) {
     case actionTypes.LOGIN_REQUEST:
@@ -71,4 +90,21 @@ export const loginRequest = () => ({
 export const loginSuccess = (payload) => ({
   type: actionTypes.LOGIN_SUCCESS,
   payload,
+});
+
+/**
+ * This is a action creator function for dispatching
+ * an action regarding to the login failure.
+ * @returns an object contains the type of action.
+ */
+export const loginError = () => ({
+  type: actionTypes.LOGIN_ERROR,
+});
+
+/**
+ * This is a action creator function for logout action
+ * @returns an object contains the type of action.
+ */
+export const logout = () => ({
+  type: actionTypes.LOGOUT,
 });
